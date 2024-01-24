3cb26088d6c7b23ddc55223f57ca7efca58dc65a
Vladimir Solovyov <leilyngale@icloud.com>
2024-01-24 14:04:13 +0800

    Basic tests in Django

--- vincent/beginner/pages/pages/tests.py
+++ vincent/beginner/pages/pages/tests.py
@@ -12,13 +12,13 @@ class HomePageTests(SimpleTestCase):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
 
-    def test_template_name_correct(self):
+    def test_temlate_name_correct(self):
         response = self.client.get(reverse("home"))
         self.assertTemplateUsed(response, "home.html")
 
     def test_template_content(self):
         response = self.client.get(reverse("home"))
-        self.assertContains(response, "<h1>Template Home Page</h1>")
+        self.assertContains(response, "<h1>Home Page</h1>")
 
 
 class AboutPageTests(SimpleTestCase):
@@ -30,28 +30,10 @@ class AboutPageTests(SimpleTestCase):
         response = self.client.get(reverse("about"))
         self.assertEqual(response.status_code, 200)
 
-    def test_template_name_correct(self):
+    def test_temlate_name_correct(self):
         response = self.client.get(reverse("about"))
         self.assertTemplateUsed(response, "about.html")
 
     def test_template_content(self):
         response = self.client.get(reverse("about"))
-        self.assertContains(response, "<h1>Template About Page</h1>")
-
-
-class ContactsPageTests(SimpleTestCase):
-    def test_url_exists_at_correct_location(self):
-        response = self.client.get("/contacts/")
-        self.assertEqual(response.status_code, 200)
-
-    def test_url_available_by_name(self):
-        response = self.client.get(reverse("contacts"))
-        self.assertEqual(response.status_code, 200)
-
-    def test_template_name_correct(self):
-        response = self.client.get(reverse("contacts"))
-        self.assertTemplateUsed(response, "contacts.html")
-
-    def test_template_content(self):
-        response = self.client.get(reverse("contacts"))
-        self.assertContains(response, "<h1>Template Contacts Page</h1>")
+        self.assertContains(response, "<h1>About Page</h1>")
--- vincent/beginner/pages/pages/views.py
+++ vincent/beginner/pages/pages/views.py
@@ -1,14 +1,11 @@
 from django.views.generic import TemplateView
 
-
 # Create your views here.
+
+
 class HomePageView(TemplateView):
     template_name = "home.html"
 
 
 class AboutPageView(TemplateView):
     template_name = "about.html"
-
-
-class ContactsPageView(TemplateView):
-    template_name = "contacts.html"
